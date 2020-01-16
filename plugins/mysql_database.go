fa32f216c16296b2bdf46ebddb6ae64312fd5a0c
tylertang <tylertang06@163.com>
2020-01-16 19:55:49 +0800

    add mysql-database plugin delete action

--- plugins/mysql_database.go
+++ plugins/mysql_database.go
@@ -12,6 +12,7 @@ var MysqlDatabasePluginActions = make(map[string]Action)
 
 func init() {
 	MysqlDatabasePluginActions["add"] = new(AddMysqlDatabaseAction)
+	MysqlDatabasePluginActions["delete"] = new(DeleteMysqlDatabaseAction)
 }
 
 type MysqlDatabasePlugin struct {
@@ -181,6 +182,146 @@ func (action *AddMysqlDatabaseAction) Do(input interface{}) (interface{}, error)
 			finalErr = err
 		}
 		output.DatabaseOwnerPassword = password
+		outputs.Outputs = append(outputs.Outputs, output)
+	}
+
+	return outputs, finalErr
+}
+
+type DeleteMysqlDatabaseAction struct {
+}
+
+type DeleteMysqlDatabaseInputs struct {
+	Inputs []DeleteMysqlDatabaseInput `json:"inputs,omitempty"`
+}
+
+type DeleteMysqlDatabaseInput struct {
+	CallBackParameter
+	Guid     string `json:"guid,omitempty"`
+	Seed     string `json:"seed,omitempty"`
+	Host     string `json:"host,omitempty"`
+	UserName string `json:"userName,omitempty"`
+	Password string `json:"password,omitempty"`
+	Port     string `json:"port,omitempty"`
+
+	// database info
+	DatabaseName      string `json:"databaseName,omitempty"`
+	DatabaseOwnerGuid string `json:"databaseOwnerGuid,omitempty"`
+	DatabaseOwnerName string `json:"databaseOwnerName,omitempty"`
+}
+
+type DeleteMysqlDatabaseOutputs struct {
+	Outputs []DeleteMysqlDatabaseOutput `json:"outputs,omitempty"`
+}
+
+type DeleteMysqlDatabaseOutput struct {
+	CallBackParameter
+	Result
+	Guid string `json:"guid,omitempty"`
+}
+
+func (action *DeleteMysqlDatabaseAction) ReadParam(param interface{}) (interface{}, error) {
+	var inputs DeleteMysqlDatabaseInputs
+	if err := UnmarshalJson(param, &inputs); err != nil {
+		return nil, err
+	}
+
+	return inputs, nil
+}
+
+func (action *DeleteMysqlDatabaseAction) deleteMysqlDatabaseCheckParam(input DeleteMysqlDatabaseInput) error {
+	if input.Host == "" {
+		return errors.New("Host is empty")
+	}
+	if input.Guid == "" {
+		return errors.New("Guid is empty")
+	}
+	if input.Seed == "" {
+		return errors.New("Seed is empty")
+	}
+	if input.UserName == "" {
+		return errors.New("UserName is empty")
+	}
+	if input.Password == "" {
+		return errors.New("Password is empty")
+	}
+	if input.Port == "" {
+		return errors.New("Port is empty")
+	}
+	if input.DatabaseName == "" {
+		return errors.New("DatabaseName is empty")
+	}
+	// if input.DatabaseOwnerGuid == "" {
+	// 	return errors.New("DatabaseOwnerGuid is empty")
+	// }
+	// if input.DatabaseOwnerName == "" {
+	// 	return errors.New("DatabaseOwnerName is empty")
+	// }
+	return nil
+}
+
+func (action *DeleteMysqlDatabaseAction) deleteMysqlDatabase(input *DeleteMysqlDatabaseInput) (output DeleteMysqlDatabaseOutput, err error) {
+	defer func() {
+		output.Guid = input.Guid
+		output.CallBackParameter.Parameter = input.CallBackParameter.Parameter
+		if err == nil {
+			output.Result.Code = RESULT_CODE_SUCCESS
+		} else {
+			output.Result.Code = RESULT_CODE_ERROR
+			output.Result.Message = err.Error()
+		}
+	}()
+
+	err = action.deleteMysqlDatabaseCheckParam(*input)
+	if err != nil {
+		return output, err
+	}
+
+	password, er := AesDePassword(input.Guid, input.Seed, input.Password)
+	if er != nil {
+		err = er
+		return output, er
+	}
+
+	if input.DatabaseOwnerName != "" {
+		if input.DatabaseOwnerGuid == "" {
+			err = errors.New("DatabaseOwnerGuid is empty")
+			return output, err
+		}
+
+		// revoke permission
+		permission := "ALL PRIVILEGES"
+		cmd := fmt.Sprintf("REVOKE %s ON %s.* FROM %s ", permission, input.DatabaseName, input.DatabaseOwnerName)
+		if err = runDatabaseCommand(input.Host, input.Port, input.UserName, password, cmd); err != nil {
+			return output, err
+		}
+
+		// delete user
+		cmd = fmt.Sprintf("DROP USER %s", input.DatabaseOwnerName)
+		if err = runDatabaseCommand(input.Host, input.Port, input.UserName, password, cmd); err != nil {
+			return output, err
+		}
+	}
+
+	// delete database
+	cmd := fmt.Sprintf("DROP DATABASE %s", input.DatabaseName)
+	if err = runDatabaseCommand(input.Host, input.Port, input.UserName, password, cmd); err != nil {
+		return output, err
+	}
+
+	return output, err
+}
+
+func (action *DeleteMysqlDatabaseAction) Do(input interface{}) (interface{}, error) {
+	inputs, _ := input.(DeleteMysqlDatabaseInputs)
+	outputs := DeleteMysqlDatabaseOutputs{}
+	var finalErr error
+
+	for _, input := range inputs.Inputs {
+		output, err := action.deleteMysqlDatabase(&input)
+		if err != nil {
+			finalErr = err
+		}
 		outputs.Outputs = append(outputs.Outputs, output)
 	}
 
