9bd89856c51093df34d44aa5e09c20d3250c248c
zgyzgyhero <894526647@qq.com>
2021-01-20 11:51:10 +0800

    Update rsa decode

--- plugins/common.go
+++ plugins/common.go
@@ -483,7 +483,7 @@ func DecryptRsa(inputString string) string {
 	}
 	inputString = inputString[4:]
 	result := inputString
-	inputBytes,err := base64.RawStdEncoding.DecodeString(inputString)
+	inputBytes,err := base64.StdEncoding.DecodeString(inputString)
 	if err != nil {
 		log.Logger.Error("Input string format to base64 fail", log.Error(err))
 		return inputString
