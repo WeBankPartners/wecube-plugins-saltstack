4f2b5bc02071a729205fd61f1250084fd22b2e64
zgyzgyhero <894526647@qq.com>
2021-06-07 19:08:26 +0800

    Add variable map json support

--- plugins/variable_replace.go
+++ plugins/variable_replace.go
@@ -2,6 +2,7 @@ package plugins
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -320,6 +321,10 @@ func getRawKeyValue(key, value, seed string) (string, string, error) {
 }
 
 func GetInputVariableMap(variable string, seed string, specialList []string) (map[string]string, error) {
+	if !strings.Contains(variable, VARIABLE_KEY_SEPERATOR) {
+		resultMap,err := GetInputVariableMapNew(variable, seed, specialList)
+		return resultMap,err
+	}
 	inputMap := make(map[string]string)
 	kvs := strings.Split(variable, VARIABLE_KEY_SEPERATOR)
 	if len(kvs) != 2 {
@@ -348,6 +353,36 @@ func GetInputVariableMap(variable string, seed string, specialList []string) (ma
 		inputMap[key] = value
 	}
 	return inputMap, nil
+}
+
+type inputVariableObj struct {
+	Key  string  `json:"key"`
+	Value string `json:"value"`
+}
+
+func GetInputVariableMapNew(variable string, seed string, specialList []string) (map[string]string, error) {
+	var kvList []*inputVariableObj
+	err := json.Unmarshal([]byte(variable), &kvList)
+	if err != nil {
+		return nil,fmt.Errorf("variableList json unmarshal fail,%s ", err.Error())
+	}
+	resultMap := make(map[string]string)
+	for _,kvObj := range kvList {
+		key, value, tmpErr := getRawKeyValue(kvObj.Key, kvObj.Value, seed)
+		if tmpErr != nil {
+			err = fmt.Errorf("Try to get decode value fail,%s ", tmpErr.Error())
+			break
+		}
+		for _,v := range specialList {
+			if strings.HasPrefix(key, v) {
+				key = key[len(v):]
+				break
+			}
+		}
+		key = strings.ToLower(key)
+		resultMap[key] = value
+	}
+	return resultMap,err
 }
 
 func CheckVariableIsAllReady(input map[string]string, variablelist []string) (err error) {
