333257f5540ded32e41e84b24955358b4aa7e19e
gaohongsong <gaohongsong@github.com>
2024-10-11 15:35:30 +0800

    feat: add single encrypt support

--- scripts/salt/rsautil.sh
+++ scripts/salt/rsautil.sh
@@ -9,5 +9,15 @@ TMP1=${TMPDIR}/RSA_TMP_ENC_SIN
 echo "$OPENSSL rsautl -encrypt -in $4 -inkey $2 -pubin -out $TMP"
 echo "$OPENSSL rsautl -sign -in $TMP -inkey $3 -out $TMP1"
 $OPENSSL rsautl -encrypt -in $4 -inkey $2 -pubin -out $TMP
-$OPENSSL rsautl -sign -in $TMP -inkey $3 -out $TMP1
+
+if [ "$1" = "enc-single" ]; then
+  echo "$OPENSSL rsautl -encrypt -in $4 -inkey $2 -pubin -out $TMP"
+  $OPENSSL rsautl -encrypt -in $4 -inkey $2 -pubin -out $TMP
+else
+  echo "$OPENSSL rsautl -encrypt -in $4 -inkey $2 -pubin -out $TMP"
+  echo "$OPENSSL rsautl -sign -in $TMP -inkey $3 -out $TMP1"
+  $OPENSSL rsautl -encrypt -in $4 -inkey $2 -pubin -out $TMP
+  $OPENSSL rsautl -sign -in $TMP -inkey $3 -out $TMP1
+fi
+
 /usr/bin/hexdump -C $TMP1|awk 'BEGIN{out="";}{for(i=2;i<NF && i<=17;i++)out = out""$i}END{print out}' > $5
