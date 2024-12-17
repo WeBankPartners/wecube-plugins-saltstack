a181f335db883a75daa0aba451ce56020d42d866
tangjiawei <tangjiawei@nnyy.com>
2024-12-17 09:28:22 +0800

    安全工单,去掉注释token代码

--- plugins/salt_api_token.go
+++ plugins/salt_api_token.go
@@ -43,8 +43,6 @@ var (
 	saltToken  SaltApiToken
 )
 
-//{"return": [{"perms": [".*"], "start": 1557300494.752942, "token": "f37858551ee3cb4f9d7f1653545e627215e1aaa5", "expire": 1557343694.752943, "user": "saltapi", "eauth": "pam"}]}
-
 type SaltApiTokenResult struct {
 	Token  string  `json:"token"`
 	Start  float64 `json:"start"`
