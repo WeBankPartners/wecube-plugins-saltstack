f49c3a12c5ef84a69974587d58717a23bf1e5130
zgyzgyhero <894526647@qq.com>
2026-04-01 17:15:28 +0800

    update check db exist

--- plugins/mysql_database.go
+++ plugins/mysql_database.go
@@ -347,8 +347,6 @@ func (action *DeleteMysqlDatabaseAction) deleteMysqlDatabase(input *DeleteMysqlD
 		return output, err
 	}
 
-	err = clearDB(input.Host, input.Port, input.UserName, password, input.DatabaseName, action.Language)
-
 	return output, err
 }
 
--- plugins/mysql_util.go
+++ plugins/mysql_util.go
@@ -108,35 +108,13 @@ func checkDBExistOrNot(host, port, loginUser, loginPwd, dbName, language string)
 		return false, getMysqlConnectError(language, err)
 	}
 
-	querySql := fmt.Sprintf("SELECT 1 FROM mysql.db WHERE Db = '%s'", dbName)
-	rows, err := DB.Query(querySql)
+	rows, err := DB.Query("SHOW DATABASES LIKE ?", dbName)
 	if err != nil {
 		return false, fmt.Errorf("Query mysql database fail,%s ", err.Error())
 	}
+	defer rows.Close()
 
 	return rows.Next(), nil
-}
-
-func clearDB(host, port, loginUser, loginPwd, dbName, language string) error {
-	// initDB param dbName = "mysql", not getUserByDB.dbName
-	err := initDB(host, port, loginUser, loginPwd, "mysql")
-	if err != nil {
-		return getMysqlConnectError(language, err)
-	}
-
-	querySql := fmt.Sprintf("SELECT 1 FROM mysql.db WHERE Db = '%s'", dbName)
-	rows, err := DB.Query(querySql)
-	if err != nil {
-		return fmt.Errorf("Query mysql database fail,%s ", err.Error())
-	}
-	if rows.Next() {
-		_, err = DB.Exec(fmt.Sprintf("DELETE FROM mysql.db WHERE Db= '%s'", dbName))
-		if err != nil {
-			return fmt.Errorf("try to delete mysql.db='%s' fail,%s ", dbName, err.Error())
-		}
-	}
-
-	return nil
 }
 
 func checkUserExistOrNot(host, port, loginUser, loginPwd, userName, language string) (bool, error) {
